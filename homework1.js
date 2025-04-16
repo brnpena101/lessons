1e7d45ee09b3209cd865c78538bbb23b02f1243f
brnpena101 <106846462+brnpena101@users.noreply.github.com>
2025-04-15 20:12:05 -0400

    Add files via upload

---
+++ homework1.js
@@ -0,0 +1,35 @@
+// Practice variables using let & const
+
+let myGoat1 = "Baby Billy";
+let myGoat2 = "Buddy";
+let myGoat3 = "Hazel";
+let myGoat4 = "Free Bird";
+
+myGoat = myGoat1 + ", " + myGoat2 + ", " + myGoat3 + ", " + myGoat4;
+// Prints out "Baby Billy, Buddy, Hazel, Free Bird"
+
+const landSpace = 20000;
+const landNeeded = 27000;
+// console.log(landNeeded - landSpace);
+//Subtract to see difference of land needed
+
+const desiredLand = 10000 * 200;
+// console.log(desiredLand);
+//Using const to multiply
+
+let nextProject = "Chicken coop, ";
+let nextOtherProject = "Chicken run";
+let bothProjects = nextProject + nextOtherProject;
+//console.log(bothProjects);
+//Example of concatenation
+
+
+
+
+
+
+
+
+
+
+
