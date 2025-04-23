ae099643dd7ba2d9819b7a10ea0e8112f9fe4393
brnpena101 <106846462+brnpena101@users.noreply.github.com>
2025-04-23 19:09:35 -0400

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -38,10 +38,24 @@ function isInRange(number) {
     }
 }
 
-let number = isInRange(25)
-console.log(number)
+//let number = isInRange(25)
+//console.log(number)
 //This is the range checker, I know you said to do it with three parameters, but I found I couldn't really figure it out that way.
 //I also used the "&" not sure if you wanted me to yet, but I tried it and it worked, I put it with the comma, but when i ran the function with the comma instead
 //cont- it gave me the right range even when it was below the minimum. 
 
 //Let me know how I did. 
+
+function trueInRange(number) {
+    if (number  < 10) {
+        return "Number is not in range"
+    } else if (number > 50) {
+        return "Number is not in range"
+    } else { return "This number is in range" }
+}
+
+let number = trueInRange(52)
+console.log(number)
+
+//Updated, I was able to figure it out today. I had to just do it in reverse order then I normally do, to be honest
+//I had to tell chatgpt to tell me the differences between if, else if, and else to get a deeper understanding how they work
