772766d4bb0f1f05ca0489e6cc18b1747258a065
brnpena101 <106846462+brnpena101@users.noreply.github.com>
2025-04-30 17:15:38 -0400

    Create homework3.js

---
+++ homework3.js
@@ -0,0 +1,45 @@
+let myNumberArray = [5, 10, 15, 20, 25];
+// console.log(myNumberArray)
+
+myNumberArray[4] = 50
+//console.log(myNumberArray)
+//Changing an array individually using the index
+
+myNumberArray[2] = 30
+// console.log(myNumberArray)
+//One more change to an array individually
+
+
+
+let starWarsPlanets = ["Tattoine", "Endor", "Naboo", "Coroscant", "Hoth"]
+// console.log(starWarsPlanets)
+//Before
+
+//After
+starWarsPlanets.push("Mustafar")
+// console.log(starWarsPlanets)
+
+//After-2
+starWarsPlanets.push("Dagobah")
+// console.log(starWarsPlanets)
+
+//Removing Dagobah
+starWarsPlanets.pop()
+// console.log(starWarsPlanets)
+
+//Adding and removing from an array using push & pop 
+
+
+
+// console.log(starWarsPlanets[4])
+//Accessing Hoth using the index
+
+// console.log(myNumberArray[0])
+//Acessing 5 using the index
+
+
+
+const myColors = ['Blue', 'Black', 'Green', 'Red']
+//console.log(myColors.toString())
+//Printing an array as a string, I assumed this is what you meant by converting it into a variable?
+
