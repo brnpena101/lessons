6c54ad8933a5033688952613300ae6138bbe7c47
brnpena101 <106846462+brnpena101@users.noreply.github.com>
2025-04-30 19:19:05 -0400

    Update homework3.js

--- homework3.js
+++ homework3.js
@@ -43,3 +43,45 @@ const myColors = ['Blue', 'Black', 'Green', 'Red']
 //console.log(myColors.toString())
 //Printing an array as a string, I assumed this is what you meant by converting it into a variable?
 
+
+function starWars() {
+    let starWarsNames = ["Nien Nunb", "Count Dooku", "Han Solo"]
+    starWarsNames.push("Greedo")
+    starWarsNames.push("R2-D2")
+    // console.log(starWarsNames)
+    }
+
+starWars()
+
+
+function jamesBond() {
+    let jamesBondActors = ["Sean Connery", "Roger Craig", "Pierce Brosnan"]
+    jamesBondActors.push("Daniel Craig")
+    jamesBondActors.push("George Lazenby")
+    // console.log(jamesBondActors)
+}
+
+jamesBond()
+
+//These are the two functions  that add names to an array dynamically
+
+
+
+
+for (let x = 1; x <= 10; x++) {
+    // console.log(x)
+}
+
+//This is the for loop that prints out numbers in order from 1-10
+
+
+function myNewNumbers() {
+    let numbers = [2, 4, 6, 8, 10]
+    for (let index = 0; index < numbers.length; index++) {
+        // console.log(numbers[index])
+    }
+}   
+
+myNewNumbers()
+
+//This is the final assignment you gave me, it printed out the items from an array using a for loop.
